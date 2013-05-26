2a9a4c41e050803f898912cbb01401c22700ae91
Chris Contolini <christopher.contolini@cfpb.gov>
2013-05-26 03:55:44 -0400

    Clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,78 +54,6 @@ module.exports = function(grunt) {
     },
 
     /**
-     * Uglify: https://github.com/gruntjs/grunt-contrib-uglify
-     * 
-     * Minify JS files.
-     * Make sure to add any other JS libraries/files you'll be using.
-     * We are excluding minified files with the final ! pattern.
-     */
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: ['static/js/jquery-1.9.1.js', 'static/js/<%= pkg.name %>.js', '!static/js/*.min.js'],
-        dest: 'static/js/<%= pkg.name %>.min.js'
-      }
-    },
-
-    /**
-     * JSHint: https://github.com/gruntjs/grunt-contrib-jshint
-     * 
-     * Validate files with JSHint.
-     * Below are options that conform to idiomatic.js standards.
-     * Feel free to add/remove your favorites: http://www.jshint.com/docs/#options
-     */
-    jshint: {
-      options: {
-        camelcase: true,
-        curly: true,
-        eqeqeq: true,
-        forin: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        quotmark: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true,
-          $: true,
-          Backbone: true,
-          _: true,
-          module: true,
-          Highcharts: true
-        }
-      },
-      all: ['static/js/<%= pkg.name %>.js']
-    },
-
-    /**
-     * Jasmine: https://github.com/gruntjs/grunt-contrib-jasmine
-     * 
-     * Run jasmine specs headlessly through PhantomJS.
-     * jQuery and Jasmine jQuery is included for your pleasure: https://github.com/velesin/jasmine-jquery
-     */
-    jasmine: {
-      src: '<%= uglify.dist.src %>',
-      options: {
-        specs: 'specs/js/*.js',
-        vendor: [
-          'specs/js/vendor/*.js'
-        ],
-        helpers: [
-          'specs/js/helpers/*.js'
-        ]
-      }
-    },
-
-    /**
      * Watch: https://github.com/gruntjs/grunt-contrib-watch
      * 
      * Run predefined tasks whenever watched file patterns are added, changed or deleted.
@@ -143,20 +71,18 @@ module.exports = function(grunt) {
    * The above tasks are loaded here.
    */
   grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-notify');
 
   /**
    * Create task aliases by registering new tasks
    */
-  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('test', ['jasmine']);
 
   /**
    * The 'default' task will run whenever `grunt` is run without specifying a task
    */
-  grunt.registerTask('default', ['test', 'recess', 'uglify']);
+  grunt.registerTask('default', ['test', 'recess']);
 
 };
